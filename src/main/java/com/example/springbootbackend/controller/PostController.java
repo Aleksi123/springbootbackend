68a16591517eb82f2d231ee0f7cb6bfb838701d3
Aleksi <a12345@live.fi>
2021-12-06 11:04:22 +0200

    PostController enabled cors
    - Added application.properties to gitignore

--- src/main/java/com/example/springbootbackend/controller/PostController.java
+++ src/main/java/com/example/springbootbackend/controller/PostController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.HashMap;
 import java.util.Map;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping(path="/api")
 public class PostController {
