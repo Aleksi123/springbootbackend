d3e819355d894f50dadda43c3ccb65a94d7fbe3e
Aleksi <a12345@live.fi>
2021-12-06 12:11:04 +0200

    Added Lombok to project
    - Refactored Post model to use Lombok

--- src/main/java/com/example/springbootbackend/model/Post.java
+++ src/main/java/com/example/springbootbackend/model/Post.java
@@ -1,10 +1,17 @@
 package com.example.springbootbackend.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 public class Post {
     @Id
@@ -12,37 +19,4 @@ public class Post {
     private Long id;
     private String title;
     private String body;
-
-    public Post(Long id, String title, String body) {
-        this.id = id;
-        this.title = title;
-        this.body = body;
-    }
-
-    public Post() {
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getBody() {
-        return body;
-    }
-
-    public void setBody(String body) {
-        this.body = body;
-    }
 }
